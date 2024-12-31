35a8aaca83b2ef7aa2be73135a83afc289c6052b
RenzoJimenezB <adrianrjb@outlook.es>
2024-12-30 20:32:21 -0500

    exception update

---
+++ src/main/java/com/demo/ecommerce/exception/AlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.demo.ecommerce.exception;
+
+public class AlreadyExistsException extends RuntimeException {
+    public AlreadyExistsException(String message) {
+        super(message);
+    }
+}
