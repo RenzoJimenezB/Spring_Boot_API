b64fc934b04673eb37396d866936e0cd925245c4
RenzoJimenezB <adrianrjb@outlook.es>
2024-12-31 12:45:12 -0500

    method return type update

--- src/main/java/com/demo/ecommerce/service/image/IImageService.java
+++ src/main/java/com/demo/ecommerce/service/image/IImageService.java
@@ -1,16 +1,17 @@
 package com.demo.ecommerce.service.image;
 
+import com.demo.ecommerce.dto.ImageDto;
 import com.demo.ecommerce.model.Image;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface IImageService {
-    Image addImage(List<MultipartFile> files, Long productId);
+    List<ImageDto> addImages(List<MultipartFile> files, Long productId);
 
     Image getImageById(Long id);
 
-    Image updateImage(MultipartFile file, Long imageId);
+    void updateImage(MultipartFile file, Long imageId);
 
     void deleteImageById(Long id);
 }
