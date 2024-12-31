5174ecaf5e31b1a6a66fcfc03763d08e244e8a0e
RenzoJimenezB <adrianrjb@outlook.es>
2024-12-30 22:51:51 -0500

    update

--- src/main/java/com/demo/ecommerce/service/image/ImageService.java
+++ src/main/java/com/demo/ecommerce/service/image/ImageService.java
@@ -1,9 +1,20 @@
 package com.demo.ecommerce.service.image;
 
+import com.demo.ecommerce.exception.ResourceNotFoundException;
 import com.demo.ecommerce.model.Image;
+import com.demo.ecommerce.repository.ImageRepository;
+import com.demo.ecommerce.service.product.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+@Service
+@RequiredArgsConstructor
 public class ImageService implements IImageService {
+
+    private final ImageRepository imageRepository;
+    private final IProductService productService;
+
     @Override
     public Image addImage(MultipartFile file, Long productId) {
         return null;
@@ -11,7 +22,8 @@ public class ImageService implements IImageService {
 
     @Override
     public Image getImageById(Long id) {
-        return null;
+        return imageRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Image not found"));
     }
 
     @Override
@@ -21,6 +33,7 @@ public class ImageService implements IImageService {
 
     @Override
     public void deleteImageById(Long id) {
-
+        Image image = getImageById(id);
+        imageRepository.delete(image);
     }
 }
