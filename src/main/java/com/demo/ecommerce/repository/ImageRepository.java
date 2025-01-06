7f2087b29089530ccdc367d765a93371f3b2f965
RenzoJimenezB <adrianrjb@outlook.es>
2025-01-06 18:55:21 -0500

    image resource delete

--- src/main/java/com/demo/ecommerce/repository/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.demo.ecommerce.repository;
-
-import com.demo.ecommerce.model.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
