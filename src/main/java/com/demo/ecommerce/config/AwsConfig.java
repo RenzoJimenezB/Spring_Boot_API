45e00952cb0749a376e60c370def2ceb6c44a68c
RenzoJimenezB <adrianrjb@outlook.es>
2025-01-06 18:56:06 -0500

    update

--- src/main/java/com/demo/ecommerce/config/AwsConfig.java
+++ src/main/java/com/demo/ecommerce/config/AwsConfig.java
@@ -3,6 +3,7 @@ package com.demo.ecommerce.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -13,7 +14,7 @@ public class AwsConfig {
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.US_EAST_1)
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(ProfileCredentialsProvider.create("s3-local-user"))
                 .build();
     }
 }
