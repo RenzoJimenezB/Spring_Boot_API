f01bc2cfc41ac6ab8636b7296a92f70150dfafa6
RenzoJimenezB <adrianrjb@outlook.es>
2025-01-13 18:55:46 -0500

    SecretManager bean

--- src/main/java/com/demo/ecommerce/config/AwsConfig.java
+++ src/main/java/com/demo/ecommerce/config/AwsConfig.java
@@ -5,16 +5,26 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 
 
 @Configuration
 public class AwsConfig {
-    
+
+
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.US_EAST_1)
                 .credentialsProvider(ProfileCredentialsProvider.create("s3-local-user"))
+                .build();
+    }
+
+
+    @Bean
+    public SecretsManagerClient secretsManagerClient() {
+        return SecretsManagerClient.builder()
+                .region(Region.US_EAST_1)
                 .build();
     }
 }
