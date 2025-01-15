3dfe5ba3b919db6990693bb7accfe339e4f6fbba
RenzoJimenezB <adrianrjb@outlook.es>
2025-01-14 19:18:17 -0500

    jwt service

--- src/main/java/com/demo/ecommerce/security/JwtService.java
+++ src/main/java/com/demo/ecommerce/security/JwtService.java
@@ -2,20 +2,44 @@ package com.demo.ecommerce.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.Map;
 
 
 @Service
+@RequiredArgsConstructor
 public class JwtService {
+
+
+    private final SecretKey secretKey;
 
 
     public String extractUsername(String token) {
         return null;
     }
 
+    public String generateToken(
+            Map<String, Object> claims,
+            UserDetails userDetails
+    ) {
+        return Jwts.builder()
+                .claims(claims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .signWith(secretKey)
+                .compact();
+    }
+
+
     private Claims extractAllClaims(String token) {
         return Jwts.parser()
-                .verifyWith(getSignInKey())
+                .verifyWith(secretKey)
                 .build()
                 .parseSignedClaims(token)
                 .getPayload();
