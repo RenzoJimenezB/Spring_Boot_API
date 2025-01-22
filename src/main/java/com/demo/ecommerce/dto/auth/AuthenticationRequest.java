c8707d6696496c55e93b2bb664a402343f3e01da
RenzoJimenezB <adrianrjb@outlook.es>
2025-01-21 23:12:08 -0500

    AuthenticationRequest

--- src/main/java/com/demo/ecommerce/dto/auth/AuthenticationRequest.java
+++ src/main/java/com/demo/ecommerce/dto/auth/AuthenticationRequest.java
@@ -1,4 +1,17 @@
 package com.demo.ecommerce.dto.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthenticationRequest {
+
+    private String email;
+    private String password;
 }
