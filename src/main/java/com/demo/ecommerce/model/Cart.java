703efd48354f7bcae08f7b7583906382378df4b6
RenzoJimenezB <adrianrjb@outlook.es>
2025-01-29 23:59:34 -0500

    update

--- src/main/java/com/demo/ecommerce/model/Cart.java
+++ src/main/java/com/demo/ecommerce/model/Cart.java
@@ -1,13 +1,12 @@
 package com.demo.ecommerce.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -20,5 +19,6 @@ public class Cart {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
+    @OneToMany()
+    private List<Product> products;
 }
