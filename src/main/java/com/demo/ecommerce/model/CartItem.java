5b9e93059aaba0d40aa5f466ba2792334cfd3d9c
RenzoJimenezB <adrianrjb@outlook.es>
2025-02-03 14:53:59 -0500

    update

--- src/main/java/com/demo/ecommerce/model/CartItem.java
+++ src/main/java/com/demo/ecommerce/model/CartItem.java
@@ -15,11 +15,11 @@ public class CartItem {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "cart_id")
+    @JoinColumn(name = "cart_id", nullable = false)
     private Cart cart;
 
     @ManyToOne
-    @JoinColumn(name = "product_id")
+    @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
     private int quantity;
