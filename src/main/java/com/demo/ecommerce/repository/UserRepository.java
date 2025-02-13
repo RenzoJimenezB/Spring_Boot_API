0026fdffa86b331dddec97dec22a1fae2d344f05
RenzoJimenezB <adrianrjb@outlook.es>
2025-02-12 19:08:45 -0500

    UserRepository custom query

--- src/main/java/com/demo/ecommerce/repository/UserRepository.java
+++ src/main/java/com/demo/ecommerce/repository/UserRepository.java
@@ -2,10 +2,16 @@ package com.demo.ecommerce.repository;
 
 import com.demo.ecommerce.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.email LIKE %:domain%")
+    List<User> searchByEmailDomain(@Param("domain") String domain);
 }
